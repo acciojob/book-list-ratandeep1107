4c11d1d3a245d6158647bbb00242c6562d6e38e2
Ratan <ratandeep1107@gmail.com>
2023-04-26 19:58:17 +0530

    first

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+let tbody = document.getElementById("book-list");
+let submit = document.getElementById("submit");
+
+let obj = {};
+let arr = [];
+
+submit.addEventListener("click", function (e) {
+  e.preventDefault();
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let isbn = document.getElementById("isbn").value;
+  let id = 0;
+  obj = { id, title, author, isbn };
+  id++;
+  arr.push(obj);
+
+  display();
+});
+function display() {
+  let nwarr = arr.map((val) => {
+    return `<tr>
+    <td>${val.title}</td>
+    <td>${val.author}</td>
+    <td>${val.isbn}</td>
+    <td class="delete" onclick="deletRow()">Del</td>
+</tr>`;
+  });
+  tbody.innerHTML = nwarr.join(" ");
+}
+
+function deletRow() {
+  let btn = document.querySelectorAll(".delete");
+  for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", function () {
+      btn[i].parentElement.remove();
+    });
+  }
+}
